e9f7913eb5247a1fc076b6132576e8702643814f
Markus Kupke <kupkemarkus@gmail.com>
2018-02-21 21:55:14 +0100

    More than one axes with different time ranges

--- tempplot.py
+++ tempplot.py
@@ -2,10 +2,15 @@
 
 import matplotlib
 matplotlib.use('svg')
+import matplotlib.pyplot
 import matplotlib.dates as mdates
 import pandas
 import os
 import time
+import datetime
+from dateutil import tz
+
+timezone = tz.tzlocal()
 
 hours = mdates.HourLocator(interval=6)
 
@@ -22,9 +27,36 @@ df = pandas.read_sql_table("t28", "mysql://%s:%s@%s/%s" %
 ds = pandas.Series(df['temperature'].values/1000.0, index=df['t'])
 ds = ds.tz_localize('UTC').tz_convert('Europe/Berlin')
 ds = ds.resample('H').mean()
+
+f = matplotlib.pyplot.gcf()
+f.set_size_inches(12.8, 19.2)
+
+matplotlib.pyplot.subplot(311)
 ax = ds.plot(grid=True)
-ax.set_title('Temperaturverlauf Dielheim, Kurpfalzstrasse 4')
+ax.set_title('Temperaturverlauf Dielheim, Kurpfalzstrasse 4, Epoche')
+matplotlib.pyplot.xlabel('Datum')
+matplotlib.pyplot.ylabel('Temperatur in Grad Celsius')
+
+matplotlib.pyplot.subplot(312)
+
+now = datetime.datetime.now(timezone)
+eightyfourhours = datetime.timedelta(hours=48)
+sevendays = datetime.timedelta(days=7)
+
+ax = ds[ds.index >= (now - eightyfourhours)].plot(grid=True)
+ax.set_title('Temperaturverlauf Dielheim, Kurpfalzstrasse 4, 48 Stunden')
 ax.xaxis.set_minor_locator(hours)
 matplotlib.pyplot.xlabel('Datum')
 matplotlib.pyplot.ylabel('Temperatur in Grad Celsius')
-matplotlib.pyplot.savefig("temperature%s.svg" % (str(time.time()),))
+
+matplotlib.pyplot.subplot(313)
+ax = ds[ds.index >= (now - sevendays)].plot(grid=True)
+ax.set_title('Temperaturverlauf Dielheim, Kurpfalzstrasse 4, 7 Tage')
+matplotlib.pyplot.xlabel('Datum')
+matplotlib.pyplot.ylabel('Temperatur in Grad Celsius')
+
+matplotlib.pyplot.tight_layout()
+
+# matplotlib.pyplot.savefig("temperature%s.svg" % (str(time.time()),), dpi=200)
+# matplotlib.pyplot.savefig("temp.svg", dpi=200)
+matplotlib.pyplot.savefig("temp.png", dpi=200)
